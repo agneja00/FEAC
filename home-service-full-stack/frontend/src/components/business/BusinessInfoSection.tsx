4980153c7bb2d176d984d42e4439ede637fa4455
agneja00 <agnieshaa00@gmail.com>
2024-12-17 22:19:29 +0200

    changed some details on the business page

--- home-service-full-stack/frontend/src/components/business/BusinessInfoSection.tsx
+++ home-service-full-stack/frontend/src/components/business/BusinessInfoSection.tsx
@@ -1,24 +1,60 @@
+import { useState } from "react";
 import styles from "./BusinessInfoSection.module.scss";
 import useCurrentBusiness from "./hooks";
 
 const BusinessInfoSection: React.FC = () => {
   const { currentBusiness } = useCurrentBusiness();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const handleImageClick = (imageUrl: string) => {
+    setSelectedImage(imageUrl);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedImage(null);
+  };
 
   return (
     <>
       <h2 className={styles.title}>Description</h2>
       <p className={styles.sectionDescription}>{currentBusiness?.about}</p>
-      <h2 className={styles.title}>Gallary</h2>
-      <div className={styles.gallary}>
-        {currentBusiness?.imageUrls.map((imageUrl, index) => (
-          <img
-            className={styles.businessImg}
-            src={imageUrl}
-            alt={`business photo ${index}`}
-            key={index}
-          />
-        ))}
+      <h2 className={styles.title}>Gallery</h2>
+      <div className={styles.gallery}>
+        {currentBusiness?.imageUrls?.length ? (
+          currentBusiness.imageUrls.map((imageUrl, index) => (
+            <img
+              className={styles.businessImg}
+              src={imageUrl}
+              alt={`${currentBusiness?.name} photo ${index + 1}`}
+              key={index}
+              onClick={() => handleImageClick(imageUrl)}
+            />
+          ))
+        ) : (
+          <p className={styles.noImages}>No images available</p>
+        )}
       </div>
+
+      {isModalOpen && selectedImage && (
+        <div className={styles.modalOverlay} onClick={handleCloseModal}>
+          <div
+            className={styles.modalContent}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <img
+              className={styles.modalImage}
+              src={selectedImage}
+              alt="Selected Image"
+            />
+            <button className={styles.closeButton} onClick={handleCloseModal}>
+              &times;
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
