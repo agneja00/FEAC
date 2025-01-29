6c0c2139231c498d201b2577aa221f3d5fc1d5f5
agneja00 <agnieshaa00@gmail.com>
2025-01-29 14:11:28 +0200

    fixed some bugs

--- home-service-full-stack/frontend/src/components/service/ServiceCard.tsx
+++ home-service-full-stack/frontend/src/components/service/ServiceCard.tsx
@@ -2,21 +2,19 @@ import { Service } from "@/components/service/types";
 import { FaRegHeart, FaHeart } from "react-icons/fa";
 import Button from "../common/Button";
 import styles from "./ServiceCard.module.scss";
-import { useToggleFavorite } from "@/components/service/hooks";
+import { useNavigate, generatePath } from "react-router-dom";
+import { ROUTES } from "@/constants/routes";
+import { toggleFavorite } from "./api";
 
 interface ServiceCardProps {
   service: Service;
   email: string;
-  isFavorite: boolean;
 }
 
-const ServiceCard = ({ service, email, isFavorite }: ServiceCardProps) => {
+const ServiceCard = ({ service, email }: ServiceCardProps) => {
   const { _id } = service;
-  const { mutate: toggleFavorite } = useToggleFavorite();
-
-  const handleToggleFavorite = () => {
-    toggleFavorite({ email, serviceId: _id });
-  };
+  const navigate = useNavigate();
+  const servicePath = generatePath(ROUTES.SERVICE_ID, { id: _id });
 
   return (
     <div className={styles.card}>
@@ -33,9 +31,19 @@ const ServiceCard = ({ service, email, isFavorite }: ServiceCardProps) => {
         <p className={styles.contactPerson}>{service.contactPerson}</p>
         <p className={styles.address}>{service.address}</p>
         <div className={styles.buttonContainer}>
-          <Button small>Book now</Button>
-          <Button favorite onClick={handleToggleFavorite}>
-            {isFavorite ? (
+          <Button small onClick={() => navigate(servicePath)}>
+            Book now
+          </Button>
+          <Button
+            favorite
+            onClick={() => toggleFavorite}
+            aria-label={
+              service.favoritedBy.includes(email)
+                ? "Remove from favorites"
+                : "Add to favorites"
+            }
+          >
+            {service.favoritedBy.includes(email) ? (
               <FaHeart fontSize={14} />
             ) : (
               <FaRegHeart fontSize={14} />
