821d4e125980aebac2f0ab0858ff6f067a80585e
agneja00 <agnieshaa00@gmail.com>
2025-02-11 06:12:33 +0200

    added the footer

---
+++ home-service-full-stack/frontend/src/components/common/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <p className={styles.footer}>
+      Copyright &#169; 2025 Agnieska Jackevic. All Rights Reserved.
+    </p>
+  );
+};
+
+export default Footer;
--- home-service-full-stack/frontend/src/components/layout/RootLayout.tsx
+++ home-service-full-stack/frontend/src/components/layout/RootLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import styles from "./Layout.module.scss";
 import Topbar from "./Topbar";
+import Footer from "../common/Footer";
 
 const RootLayout = () => {
   return (
@@ -9,6 +10,7 @@ const RootLayout = () => {
       <div className={styles.rootContainer}>
         <Outlet />
       </div>
+      <Footer />
     </>
   );
 };
