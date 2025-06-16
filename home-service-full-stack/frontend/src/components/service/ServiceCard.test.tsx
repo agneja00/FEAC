209d1b99dda772457c4c23c87097c89488b8f41a
agneja00 <agnieshaa00@gmail.com>
2025-06-17 00:13:01 +0300

    updated ServiceCard test

--- home-service-full-stack/frontend/src/components/service/ServiceCard.test.tsx
+++ home-service-full-stack/frontend/src/components/service/ServiceCard.test.tsx
@@ -36,6 +36,7 @@ const mockUserContextValue = {
   isLoggedIn: true,
   login: jest.fn(),
   logout: jest.fn(),
+  setUser: jest.fn(),
 };
 
 const mockService: IService = {
@@ -69,7 +70,7 @@ describe("ServiceCard", () => {
   });
 
   const renderServiceCard = (
-    props: Partial<React.ComponentProps<typeof ServiceCard>> = {},
+    props: Partial<React.ComponentProps<typeof ServiceCard>> = {}
   ) => {
     const defaultProps = {
       service: mockService,
@@ -81,7 +82,7 @@ describe("ServiceCard", () => {
         <UserContext.Provider value={mockUserContextValue}>
           <ServiceCard {...defaultProps} {...props} />
         </UserContext.Provider>
-      </MemoryRouter>,
+      </MemoryRouter>
     );
   };
 
@@ -137,7 +138,7 @@ describe("ServiceCard", () => {
   it("renders the favorite button", () => {
     renderServiceCard();
     expect(
-      screen.getByRole("button", { name: /Add to favorites/i }),
+      screen.getByRole("button", { name: /Add to favorites/i })
     ).toBeInTheDocument();
   });
 
@@ -154,7 +155,7 @@ describe("ServiceCard", () => {
 
     expect(mutate).toHaveBeenCalledWith(
       { email: mockUser.email, serviceId: mockService._id, lang: "en" },
-      expect.anything(),
+      expect.anything()
     );
   });
 
