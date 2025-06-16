5d59f1920ac80224eddb6d7fbd1f70e6940e2293
agneja00 <agnieshaa00@gmail.com>
2025-06-16 23:47:02 +0300

    updated Topbar test

--- home-service-full-stack/frontend/src/components/layout/Topbar.test.tsx
+++ home-service-full-stack/frontend/src/components/layout/Topbar.test.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { useParams, useNavigate, generatePath } from "react-router-dom";
 import { UserContext } from "@/components/context/UserContext";
 import { useTranslation } from "react-i18next";
@@ -27,7 +26,7 @@ jest.mock("../common/LanguageSwither", () => ({
 jest.mock("@/components/common/Modal", () => ({
   __esModule: true,
   default: jest.fn(({ isOpen, children }) =>
-    isOpen ? <div>{children}</div> : null,
+    isOpen ? <div>{children}</div> : null
   ),
 }));
 
@@ -40,6 +39,7 @@ describe("Topbar", () => {
   const navigate = jest.fn();
   const t = jest.fn((key) => key);
   const logout = jest.fn();
+  const setUser = jest.fn();
 
   const user = {
     _id: "123",
@@ -51,18 +51,42 @@ describe("Topbar", () => {
     mockUseParams.mockReturnValue({ lang: "en" });
     mockUseNavigate.mockReturnValue(navigate);
     mockUseTranslation.mockReturnValue({ t });
+
     mockGeneratePath.mockImplementation((path, params) => {
-      if (path === ROUTES.HOME) return "/en";
-      if (path === ROUTES.SERVICES) return "/en/services";
-      if (path === ROUTES.ABOUT_US) return "/en/about-us";
-      if (path === ROUTES.FOR_BUSINESS_PARTNERS)
-        return "/en/for-business-partners";
-      if (path === ROUTES.BOOKINGS_FILTER)
-        return `/en/bookings/${params.email}/${params.status}`;
-      if (path === ROUTES.FAVORITES)
-        return `/en/services/${params.email}/favorites`;
-      if (path === ROUTES.LOGIN) return "/en/auth/login";
-      return path;
+      switch (path) {
+        case ROUTES.HOME:
+          return `/${params.lang}`;
+        case ROUTES.SERVICES:
+          return `/${params.lang}/services`;
+        case ROUTES.SERVICES_CATEGORY:
+          return `/${params.lang}/services/categories/${params.category}`;
+        case ROUTES.SERVICE_ID:
+          return `/${params.lang}/service/${params.id}`;
+        case ROUTES.ABOUT_US:
+          return `/${params.lang}/about-us`;
+        case ROUTES.FOR_BUSINESS_PARTNERS:
+          return `/${params.lang}/for-business-partners`;
+        case ROUTES.LOGIN:
+          return `/${params.lang}/auth/login`;
+        case ROUTES.REGISTER:
+          return `/${params.lang}/auth/register`;
+        case ROUTES.ACCOUNT:
+          return `/${params.lang}/user/${params.email}`;
+        case ROUTES.BOOKINGS:
+          return `/${params.lang}/bookings/user/${params.email}`;
+        case ROUTES.BOOKINGS_FILTER:
+          return `/${params.lang}/bookings/user/${params.email}/${params.status}`;
+        case ROUTES.FAVORITES:
+          return `/${params.lang}/services/user/${params.email}/favorites`;
+        case ROUTES.FAVORITES_CATEGORY:
+          return `/${params.lang}/services/user/${params.email}/favorites/${params.category}`;
+        case ROUTES.ERROR_LANG:
+          return `/${params.lang}/error`;
+        case ROUTES.ERROR_GLOBAL:
+          return `/error`;
+        default:
+          return "/";
+      }
     });
   });
 
@@ -70,14 +94,24 @@ describe("Topbar", () => {
     jest.clearAllMocks();
   });
 
-  it("renders the Topbar with navigation links", () => {
-    render(
-      <UserContext.Provider
-        value={{ user: null, isLoggedIn: false, login: jest.fn(), logout }}
-      >
+  const renderWithUserContext = (
+    ctxValue: React.ContextType<typeof UserContext>
+  ) => {
+    return render(
+      <UserContext.Provider value={ctxValue}>
         <Topbar />
-      </UserContext.Provider>,
+      </UserContext.Provider>
     );
+  };
+
+  it("renders the Topbar with navigation links", () => {
+    renderWithUserContext({
+      user: null,
+      isLoggedIn: false,
+      login: jest.fn(),
+      logout,
+      setUser,
+    });
 
     expect(screen.getByText("topbar.services")).toBeInTheDocument();
     expect(screen.getByText("common.aboutUs")).toBeInTheDocument();
@@ -85,42 +119,45 @@ describe("Topbar", () => {
   });
 
   it("renders the login button when user is not logged in", () => {
-    render(
-      <UserContext.Provider
-        value={{ user: null, isLoggedIn: false, login: jest.fn(), logout }}
-      >
-        <Topbar />
-      </UserContext.Provider>,
-    );
+    renderWithUserContext({
+      user: null,
+      isLoggedIn: false,
+      login: jest.fn(),
+      logout,
+      setUser,
+    });
 
     expect(screen.getByText("buttons.loginOrSign")).toBeInTheDocument();
   });
 
   it("renders the avatar and user menu when user is logged in", () => {
-    render(
-      <UserContext.Provider
-        value={{ user, isLoggedIn: true, login: jest.fn(), logout }}
-      >
-        <Topbar />
-      </UserContext.Provider>,
-    );
+    renderWithUserContext({
+      user,
+      isLoggedIn: true,
+      login: jest.fn(),
+      logout,
+      setUser,
+    });
 
     expect(screen.getByText(user.email[0])).toBeInTheDocument();
   });
 
   it("toggles the mobile menu when the menu icon is clicked", () => {
-    render(
-      <UserContext.Provider
-        value={{ user: null, isLoggedIn: false, login: jest.fn(), logout }}
-      >
-        <Topbar />
-      </UserContext.Provider>,
-    );
+    renderWithUserContext({
+      user: null,
+      isLoggedIn: false,
+      login: jest.fn(),
+      logout,
+      setUser,
+    });
 
-    fireEvent.click(screen.getByLabelText("Open navigation menu"));
-    expect(screen.getByLabelText("Close navigation menu")).toBeInTheDocument();
+    const openMenuButton = screen.getByLabelText("Open navigation menu");
+    fireEvent.click(openMenuButton);
 
-    fireEvent.click(screen.getByLabelText("Close navigation menu"));
+    const closeMenuButton = screen.getByLabelText("Close navigation menu");
+    expect(closeMenuButton).toBeInTheDocument();
+
+    fireEvent.click(closeMenuButton);
     expect(screen.getByLabelText("Open navigation menu")).toBeInTheDocument();
   });
 });
