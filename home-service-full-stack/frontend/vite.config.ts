d66127661197b07c32a02b018212a49b76076bb1
agneja00 <agnieshaa00@gmail.com>
2025-09-26 01:32:01 +0300

    Fixed problem with heroku build #2

--- home-service-full-stack/frontend/vite.config.ts
+++ home-service-full-stack/frontend/vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "./src/styles/variables" as *;`,
+        additionalData: `@use "@/styles/variables" as *;`,
       },
     },
   },
