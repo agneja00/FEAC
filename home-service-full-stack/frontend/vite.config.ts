f32017e4bf2ba748aed1be7a43eed577e2b31243
agneja00 <agnieshaa00@gmail.com>
2025-10-05 16:20:21 +0300

    Added support for older browsers

--- home-service-full-stack/frontend/vite.config.ts
+++ home-service-full-stack/frontend/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    legacy({
+      targets: ["> 0.01%"],
+    }),
+  ],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
