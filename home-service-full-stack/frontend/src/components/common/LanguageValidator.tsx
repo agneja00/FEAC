d38ebad61025c7f27626b607943df7ff2f462582
agneja00 <agnieshaa00@gmail.com>
2026-08-02 00:45:16 +0300

    Add new logic to LanguageValidator

--- home-service-full-stack/frontend/src/components/common/LanguageValidator.tsx
+++ home-service-full-stack/frontend/src/components/common/LanguageValidator.tsx
@@ -1,4 +1,6 @@
 import { useParams, Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 const supportedLanguages = ["en", "lt", "ru"];
@@ -6,6 +8,16 @@ const supportedLanguages = ["en", "lt", "ru"];
 const LanguageValidator = () => {
   const { lang } = useParams<{ lang: string }>();
   const currentLang = lang || "en";
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    if (
+      supportedLanguages.includes(currentLang) &&
+      i18n.language !== currentLang
+    ) {
+      i18n.changeLanguage(currentLang);
+    }
+  }, [currentLang, i18n]);
 
   if (!supportedLanguages.includes(currentLang)) {
     return <ErrorPage />;
